3c59bef5bf5356804193037fdf724b6cb3fc078f
Abhaykahar <ak2415112@gmail.com>
2024-10-05 03:42:13 +0530

    recipe

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="bg-dark text-light p-3">
+            <h1 className="text-center">Recipe Management App</h1>
+        </header>
+    );
+};
+
+export default Header;
